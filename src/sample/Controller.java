7a34e2076511a5245ac57e9b51e8182b2d1055d3
Lolitron <Ovcharov_05@mail.ru>
2021-09-12 23:58:25 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public ChoiceBox n1Choose;
+    public Button btnGener;
+    public Slider scaleChooser;
+    public ChoiceBox n2Choose;
+
+
+    public void onClick(ActionEvent actionEvent) throws InterruptedException, MidiUnavailableException {
+        Weights.generate((int)n1Choose.getValue(),(int)n2Choose.getValue(),(int)scaleChooser.getValue());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Integer> cursors = FXCollections.observableArrayList(60, 62, 64,65,67,69);
+        n1Choose.setItems(cursors);
+        n2Choose.setItems(cursors);
+        n1Choose.setValue(cursors.get(0));
+        n2Choose.setValue(cursors.get(0));
+        System.out.println(n1Choose.getValue());
+        try {
+            Weights.learnFromSong("C:\\Users\\ClarVik\\source\\repos\\src\\com\\company\\New MIDI File 1.mid");
+            Weights.learnFromSong("C:\\Users\\ClarVik\\source\\repos\\src\\com\\company\\New MIDI File 2.mid");
+            Weights.learnFromSong("C:\\Users\\ClarVik\\source\\repos\\src\\com\\company\\New MIDI File 3.mid");
+            Weights.learnFromSong("C:\\Users\\ClarVik\\source\\repos\\src\\com\\company\\New MIDI File 4.mid");
+            Weights.learnFromSong("C:\\Users\\ClarVik\\source\\repos\\src\\com\\company\\New MIDI File 5.mid");
+        } catch (InvalidMidiDataException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/sample/Weights.java
@@ -0,0 +1,123 @@
+package sample;
+
+import javax.sound.midi.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Weights
+{
+    private static float[][][] weights= new float[12][12][12];
+
+    public static void updateWeights(int n1,int n2,int n3){
+        weights[n1-60][n2-60][n3-60]++;
+    }
+
+    public static int sumAll(int n1,int n2) {
+        int sum=0;
+        for(int i=0;i<12;sum+=weights[n1][n2][i],i++);
+        return sum;
+    }
+
+    public static void normalize()
+    {
+        int sum;
+
+        for(int i=0;i<12;i++)
+        {
+            for(int j=0;j<12;j++) {
+                sum = sumAll(i,j);
+                if (sum != 0)
+                    for (int n = 0; n < 12; n++)
+                        weights[i][j][n] /= sum;
+            }
+        }
+    }
+
+
+    public static void learnFromSong(String midiFile) throws InvalidMidiDataException, IOException {
+        Sequence sequence = MidiSystem.getSequence(new File(midiFile));
+        MidiMessage message;
+        int key;
+
+        int id = 0;
+        int[] noteArray=new int[2];
+
+        for(Track track : sequence.getTracks())
+        {
+            for(int i=0;i< track.size();i++)
+            {
+                message = track.get(i).getMessage();
+
+                if(message instanceof ShortMessage)
+                {
+                    ShortMessage sm=(ShortMessage)message;
+
+                    if(sm.getCommand() == ShortMessage.NOTE_ON)
+                    {
+                        key=sm.getData1();
+
+                        if(id==2)
+                        {
+                            updateWeights(noteArray[0],noteArray[1],key);
+                            noteArray[0]=noteArray[1];
+                            noteArray[1]=key;
+                        }
+                        else
+                            noteArray[id++]=key;
+                    }
+                }
+            }
+        }
+
+        Weights.normalize();
+
+    }
+
+
+
+
+    public static void generate(int n1,int n2,int ton) throws MidiUnavailableException, InterruptedException {
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+        final MidiChannel[] channels = synth.getChannels();
+
+        int fn=n1,sn=n2,nn,tr;
+
+        for(int i=0;i<33;i++)
+        {
+            nn=Weights.nextNote(fn,sn);
+
+            channels[0].noteOn(nn+ton,127);
+            tr=nn+4;
+            if(Math.random()<0.5) channels[0].noteOn(tr+ton,127);
+            channels[0].noteOn(tr+3+ton,127);
+            if(Math.random()<0.2) channels[0].noteOn(tr+8+ton,127);
+            if (i%16==0 && i!=0)
+                Thread.sleep(1000);
+            else if(i%2==0)
+                Thread.sleep(350);
+            else
+                Thread.sleep(150);
+            channels[0].noteOff(nn+ton);
+            channels[0].noteOff(tr+ton);
+            channels[0].noteOff(tr+3+ton);
+            channels[0].noteOff(tr+8+ton);
+            fn=sn;
+            sn=nn;
+        }
+    }
+
+
+    public static int nextNote(int n1,int n2) {
+        double rnd,sum=0;
+        rnd=Math.random();
+        for(int i=0;i<12;i++)
+        {
+            sum+=weights[n1-60][n2-60][i];
+
+            if(rnd<=sum)
+                return i+60;
+        }
+        return 62;
+    }
+}
